56de09cdf6d403044e25fd92d56ffef01693219c
trannhantam4 <80136734+trannhantam4@users.noreply.github.com>
2023-08-21 20:15:52 +0700

    localFunc

---
+++ screens/AllPlaces.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function AllPlaces() {
+  return (
+    <View>
+      <Text>AllPlaces</Text>
+    </View>
+  );
+}
